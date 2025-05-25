d80e544880c2224ac048ea6d587037e6b4c9a9bb
Kostiantyn Krykun <kostiantyn_krykun@epam.com>
2025-05-26 00:30:02 +0300

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "KostiantynKrykun-org"
 
     workspaces {
       name = "learn-terraform"
     }
   }
+*/
 
   required_providers {
     aws = {
